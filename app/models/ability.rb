1116594a6ac151239a865850bb71ad35b7efb9c0
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-01 10:50:21 +0100

    add ability model and define priviledges

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,17 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    user ||= User.new # guest user (not logged in)
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Appointment
+      can :destroy, Appointment do |appointment|
+        appointment.user_id == user.id
+      end
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
