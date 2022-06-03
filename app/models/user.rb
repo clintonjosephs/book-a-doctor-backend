e21da23cd3933a005e26e7f302b1c90f88b124b1
jihaneh <jijih@hotmail.fr>
2022-06-03 15:23:34 +0200

    Fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   # validations
   validates :name, presence: true, length: { in: 3..150 }
   validates :password, presence: true, length: { in: 6..20 }
-  
+
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
   end
