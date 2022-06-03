ad9db34f44dcfde071498bffbaf17e56d540e593
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-03 14:19:37 +0100

    integration test for the login api

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -18,7 +18,7 @@ class V1::UsersController < ApplicationController
         render json: { error: 'unauthorized', error_message: ['invalid password'] }, status: :unauthorized
       end
     else
-      render json: { error: 'unauthorized', error_message: @user.errors }, status: :unauthorized
+      render json: { error: 'unauthorized', error_message: ['User does not exit'] }, status: :unauthorized
     end
   end
 
