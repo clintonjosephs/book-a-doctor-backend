0647ca31c93b8d7bd5d96b95d2f4d9b54b47dc36
Nuri1977 <nurilacka@gmail.com>
2022-06-06 15:24:17 +0200

    Update doctor#create action to accept admin requests, save data to database and give back responses

--- app/controllers/v1/doctors_controller.rb
+++ app/controllers/v1/doctors_controller.rb
@@ -1,4 +1,6 @@
 class V1::DoctorsController < ApplicationController
+  before_action :authorize_request, only: %i[index show create destroy]
+
   def index; end
 
   def show
@@ -6,7 +8,24 @@ class V1::DoctorsController < ApplicationController
     render json: DoctorSerializer.new(@doctor).serializable_hash[:data][:attributes], status: :ok
   end
 
-  def new; end
+  def create
+    if @current_user.role == 'admin'
+      @doctor = Doctor.new(doctor_params)
+      if @doctor.save
+        render json: DoctorSerializer.new(@doctor).serializable_hash[:data][:attributes], status: :created
+      else
+        render json: @doctor.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'unauthorized', error_message: 'you need admin permision' }, status: :unauthorized
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialization, :city, :description, :cost_per_day, :image, :image_url)
+  end
 end
