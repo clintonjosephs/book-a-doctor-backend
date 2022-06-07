bebf50eab684f403ba83a2e6ac821a9aae82f10d
jihaneh <jijih@hotmail.fr>
2022-06-07 18:16:59 +0200

    Update Appointment and add tests

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Ally', email: 'test@test.com', password: '123456') }
+  let(:doctor) { create(:doctor) }
+  subject { described_class.new(date_of_appointment: '2000-10-10', user: user, doctor: doctor) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a date_of_appointment' do
+    subject.date_of_appointment = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a user' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a doctor' do
+    subject.doctor = nil
+    expect(subject).to_not be_valid
+  end
+
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:doctor) }
+  end
 end
