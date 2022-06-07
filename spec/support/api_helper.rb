f781c397dd84d31050b96e5a6ae7449330635c68
Nuri1977 <nurilacka@gmail.com>
2022-06-07 10:20:29 +0200

    Remove api_helper method

--- spec/support/api_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApiHelper
-  def authenticated_header(request, user)
-    token = Knock::AuthToken.new(payload: { sub: user.id }).token
-    request.headers.merge!(Authorization: "Bearer #{token}")
-  end
-end
