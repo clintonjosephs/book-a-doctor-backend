4ce2c7dd662acf0e24589ca275779d019db4a253
jihaneh <jijih@hotmail.fr>
2022-06-07 18:54:30 +0200

    Update methode create in Appointment controller and Add tests

--- app/controllers/v1/appointments_controller.rb
+++ app/controllers/v1/appointments_controller.rb
@@ -5,7 +5,22 @@ class V1::AppointmentsController < ApplicationController
 
   def show; end
 
-  def create; end
+  def create
+    appointment = Appointment.new(appointment_params)
+    if appointment.save
+      render json: { doctor_id: appointment.doctor_id,
+                     date_of_appointment: appointment.date_of_appointment.strftime('%A, %d %B %Y') },
+             status: :created
+    else
+      render json: appointment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
 
   def destroy; end
+
+  def appointment_params
+    post_params = params.permit(:doctor_id, :date_of_appointment)
+    post_params[:user_id] = @current_user.id
+    post_params
+  end
 end
