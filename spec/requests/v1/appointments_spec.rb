6e0bdddadc9ed10c1cd99d554b196dd7304c221f
jihaneh <jijih@hotmail.fr>
2022-06-13 12:28:08 +0200

    Add Delete Appointment tests

--- spec/requests/v1/appointments_spec.rb
+++ spec/requests/v1/appointments_spec.rb
@@ -24,6 +24,14 @@ RSpec.describe 'V1::Appointments', type: :request do
       expect(json['message']).to eq(['All appointments loaded'])
       expect(response).to have_http_status(:ok)
     end
+
+    it 'returns an error' do
+      Appointment.destroy_all
+      get '/v1/appointments', headers: { 'Authorization' => "Bearer #{user_token}" }
+      json = JSON.parse(response.body)
+      expect(json['error_message']).to eq(['No appointments found'])
+      expect(response).to have_http_status(:not_found)
+    end
   end
 
   describe 'GET /appointments/:id' do
@@ -36,9 +44,43 @@ RSpec.describe 'V1::Appointments', type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
   let(:doctor) do
     Doctor.create(name: 'Dr. John Doe', city: 'New York',
                   specialization: 'Cardiology', cost_per_day: 100, description: 'Dr. John Doe is a cardiologist.')
+  end
+
+  describe 'DELETE v1/appointments/:id' do
+    it 'delete appointment' do
+      user = User.create(name: 'test', email: 'emaildeletee@gmail.com', password: '123456')
+      appointment = Appointment.create(date_of_appointment: '2019-01-01', doctor_id: doctor.id, user_id: user.id)
+      post '/v1/users/login', params: { email: user.email, password: user.password }
+
+      delete "/v1/appointments/#{appointment.id}", headers: { 'Authorization' => "Bearer #{json['token']}" }
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'DELETE v1/appointments/:id' do
+    it 'can not delete appointment (bad user)' do
+      appointment = Appointment.create(date_of_appointment: '2019-01-01', doctor_id: doctor.id, user_id: 1)
+
+      delete "/v1/appointments/#{appointment.id}", headers: { 'Authorization' => "Bearer #{access_token}" }
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'DELETE v1/appointments/:id' do
+    it 'can not delete appointment (bas appointment)' do
+      appointment = Appointment.create(date_of_appointment: '2019-01-01', doctor_id: doctor.id, user_id: 1)
+      appointment.delete
+
+      delete "/v1/appointments/#{appointment.id}", headers: { 'Authorization' => "Bearer #{access_token}" }
+
+      expect(response).to have_http_status(:not_found)
+    end
   end
 
   describe 'POST v1/appointments' do
